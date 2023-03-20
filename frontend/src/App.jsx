903cf555160ffe681b9aea64b9863c487cbb3eb2
itmakesmesoft <dmsgur7112@gmail.com>
2023-03-20 15:27:22 +0900

    Fix: 버그 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css'
+import SignupPage  from './pages/SignupPage/index'
+import LoginPage  from './pages/LoginPage/index'
 import NavBar from './components/NavBar'
+import './App.css'
 
 function App() {
-
   return (
-    <>
+    <div className="App">
       <NavBar/>
-      <div className="App">
-      </div>
-    </>
-
+      <SignupPage/>
+      <LoginPage/>
+    </div>
   )
 }
 
