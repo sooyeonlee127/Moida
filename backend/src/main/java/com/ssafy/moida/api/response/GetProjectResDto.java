c02e05d5e9258623a522e3f08005d2ac3d1e57ef
limse0204 <limse0204@naver.com>
2023-03-21 15:30:49 +0900

    Feat: 프로젝트 조회 dto 수정

--- backend/src/main/java/com/ssafy/moida/api/response/GetProjectResDto.java
+++ backend/src/main/java/com/ssafy/moida/api/response/GetProjectResDto.java
@@ -1,25 +1,24 @@
 package com.ssafy.moida.api.response;
 
 import com.ssafy.moida.api.common.DonationDto;
+import com.ssafy.moida.api.common.ProjectDto;
 import java.util.List;
 import lombok.Getter;
 
 /**
  * [프로젝트 조회 Dto : 메인페이지에 필요한 데이터]
- * 프로젝트 고유 ID, 현재 모금액, DonationDto(봉사일자(시작-종료), 위치, 설명), 사진
+ * ProjectDto(프로젝트) : 프로젝트 아이디, 카테고리, 주제, 차수, 간단설명
+ * DonationDto(기부) : 기부 일자(시작-종료), 모금액(현재, 목표), 설명
+ * 사진
  */
 @Getter
 public class GetProjectResDto {
-    private Long projectId;
-    private Long amount;
+    private ProjectDto projectDto;
     private DonationDto donationDto;
-
-    // 사진
     private List<String> pictures;
 
-    public GetProjectResDto(long projectId, long amount, DonationDto donationDto, List<String> pictures) {
-        this.projectId = projectId;
-        this.amount = amount;
+    public GetProjectResDto(ProjectDto projectDto, DonationDto donationDto, List<String> pictures) {
+        this.projectDto = projectDto;
         this.donationDto = donationDto;
         this.pictures = pictures;
     }
