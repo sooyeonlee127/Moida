6cae441202319e6d1b1d1b869f59a946ba1c3447
limse0204 <limse0204@naver.com>
2023-03-21 17:17:44 +0900

    Feat: rest api url 변경

--- backend/src/main/java/com/ssafy/moida/api/controller/ProjectController.java
+++ backend/src/main/java/com/ssafy/moida/api/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.ssafy.moida.api.controller;
 
 import com.ssafy.moida.api.request.CreateProjectReqDto;
+import com.ssafy.moida.api.response.GetProjectResDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
@@ -12,23 +13,25 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name="프로젝트(관리자 ver.)")
 @RestController
-@RequestMapping("/api/project")
+@RequestMapping("/project")
 public class ProjectController {
-    @Operation(summary = "프로젝트 생성", description = "기부, 봉사가 포함된 새 프로젝트를 생성합니다.")
-    @PostMapping(path = "/", consumes = {
+    @Operation(summary = "프로젝트 생성", description = "새 프로젝트를 생성합니다.")
+    @PostMapping(consumes = {
         MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE
     })
     public ResponseEntity<?> createProject(
         @RequestPart(value = "info", required = true) CreateProjectReqDto createProjectReqDto,
         @RequestPart(value = "files", required = false) List<MultipartFile> files
     ){
+        System.out.println(createProjectReqDto);
         return new ResponseEntity<>("프로젝트 생성 완료", HttpStatus.OK);
     }
 
     @Operation(summary = "프로젝트 정보 조회", description = "메인페이지에서 조회할 프로젝트의 가벼운 정보를 조회합니다.")
-    @GetMapping("/")
-    public ResponseEntity<?> getProject(){
-        return new ResponseEntity<>("프로젝트 조회 완료", HttpStatus.OK);
+    @GetMapping
+    public GetProjectResDto getProject(){
+
+        return null;
     }
 
     @Operation(summary = "프로젝트 정보 상세 조회", description = "프로젝트 상세 페이지 정보를 조회합니다.")
