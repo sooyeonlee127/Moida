47eca8465104945a07b26b65432bb3bdfb09d22c
itmakesmesoft <dmsgur7112@gmail.com>
2023-03-21 17:58:18 +0900

    Feat: 기부 페이지 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import NavBar from './components/NavBar'
+// import NavBar from './components/NavBar'
 import DonationPage from './pages/DonationPage/index'
 import './App.css'
 
 
+
 function App() {
+
   return (
     <div className="App">
-      <NavBar/>
+      {/* <NavBar/> */}
       <DonationPage/>
     </div>
 
--- frontend/src/pages/DonationPage/index.jsx
+++ frontend/src/pages/DonationPage/index.jsx
@@ -1,26 +1,90 @@
 import styled from "styled-components"
 import tw from "twin.macro"
 
-
 const DonationPage = () => {
+    const ratio = 50;
+    // const ratio = parseInt(모금액/목표금액*100) => 반환값 0~100사이 정수
+
     return (
         <div>
-            <h1>donation page</h1>
-            <main className="container mx-auto px-4">
-                <MainImage></MainImage>
-                <div>
-                    <div></div>
-                </div>
-            </main>
-            <div></div>
+            <Main>
+                <MainImage />
+                <Aside>
+                    <TabGroup>
+                        <Tab>기부하기</Tab>
+                        <Tab>봉사하기</Tab>
+                    </TabGroup>
+                    <div>
+                        <span>D-day</span>
+                        <p>Title</p>
+                        <p>content</p>
+                        <div>
+                            <p>{ratio}%</p>
+                            기부 목표금액<br/>
+                            <progress id="progress" value={ratio} min="0" max="100" />
+                            현재 모금액
+                        </div>
+                        <div>
+                            <p>10000 원</p>
+                            <CoinButtonGroup>
+                                <CoinButton>1,000원</CoinButton>
+                                <CoinButton>5,000원</CoinButton>
+                                <CoinButton>10,000원</CoinButton>
+                                <CoinButton>50,000원</CoinButton>
+                            </CoinButtonGroup>
+                        </div>
+                        <GroupButton>
+                            <Button>초기화</Button>
+                            <Button>기부하기</Button>
+                        </GroupButton>
+                    </div>
+                </Aside>
+            </Main>
+            <Article />
         </div>
     )
 }
 
-const MainImage = styled.div`
-${tw`h-96 w-64 bg-cover bg-[url('https://thumb.mt.co.kr/06/2021/02/2021022514598215872_1.jpg/dims/optimize/')]`}
+const Main = styled.div`
+padding-top: 50px;
+${tw`container flex flex-row space-x-5 mx-auto`}
 `
 
+const MainImage = styled.div`
+height: 500px;
+${tw`basis-3/5 bg-cover bg-[url('https://thumb.mt.co.kr/06/2021/02/2021022514598215872_1.jpg/dims/optimize/')]`}
+`
 
+const Aside = styled.div`
+${tw`border basis-2/5`}
+`
+const TabGroup = styled.div`
+border-color: black;
+${tw`flex flex-row ring-1 ring-current divide-x`}
+`
+
+const Tab = styled.button`
+${tw`basis-1/2 px-2 py-3 hover:bg-sky-500 active:bg-sky-600`}
+`
+
+const CoinButtonGroup = styled.div`
+${tw`grid grid-cols-4 gap-1 `}
+`
+
+const CoinButton = styled.button`
+${tw`border px-3 py-1 hover:bg-sky-500 active:bg-sky-600`}
+`
+
+const GroupButton = styled.button`
+${tw`flex space-x-3`}
+`
+
+const Button = styled.button`
+${tw`border px-2 py-2`}
+`
+
+const Article = styled.div`
+${tw`py-5 px-3 border mt-10 w-full`}
+`
 
 export default DonationPage
