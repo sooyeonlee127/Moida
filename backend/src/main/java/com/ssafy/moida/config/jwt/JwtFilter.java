d5765a71188a6b901b9075e801c93cd908158453
한선영 <gyeoul98@naver.com>
2023-03-21 16:16:56 +0900

    Feat: Jwt Filter 추가 및 security config 설정 변경

--- backend/src/main/java/com/ssafy/moida/config/jwt/JwtFilter.java
+++ backend/src/main/java/com/ssafy/moida/config/jwt/JwtFilter.java
@@ -1,4 +1,44 @@
 package com.ssafy.moida.config.jwt;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = resolveToken(request);
+
+        if (StringUtils.hasText(jwt) && jwtTokenProvider.validateToken(jwt)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.info("set Authentication to security context for '{}', uri: {}", authentication.getName(), request.getRequestURI());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    // Request Header 에서 토큰 정보를 꺼내오기
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(JwtProperties.AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(JwtProperties.TOKEN_PREFIX)) {
+            return bearerToken.substring(JwtProperties.TOKEN_PREFIX.length());
+        }
+        return null;
+    }
+
 }
