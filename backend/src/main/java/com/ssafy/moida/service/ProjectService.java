860e36f82e2598c3a1606df0080f5948a399aaf1
LIM SEEUN <ssen.lim@gmail.com>
2023-03-22 06:02:40 +0900

    Feat: findById 반환값 변경

--- backend/src/main/java/com/ssafy/moida/service/ProjectService.java
+++ backend/src/main/java/com/ssafy/moida/service/ProjectService.java
@@ -4,12 +4,16 @@ import com.ssafy.moida.api.common.DonationDto;
 import com.ssafy.moida.api.common.ProjectDto;
 import com.ssafy.moida.api.common.VolunteerDto;
 import com.ssafy.moida.api.request.CreateProjectReqDto;
+import com.ssafy.moida.api.response.GetProjectDetailResDto;
 import com.ssafy.moida.api.response.GetProjectResDto;
+import com.ssafy.moida.model.Category;
 import com.ssafy.moida.model.Project;
 import com.ssafy.moida.model.ProjectDonation;
 import com.ssafy.moida.model.ProjectVolunteer;
 import com.ssafy.moida.repository.ProjectRepository;
 import com.ssafy.moida.repository.VolunteerRepository;
+
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -35,12 +39,12 @@ public class ProjectService {
         // 봉사 데이터베이스에 저장
         ProjectVolunteer projectVolunteer = volunteerService.save(createProjectReqDto.getVolunteerDto());
 
-
         // 프로젝트 데이터베이스에 저장
         Project project = Project.builder()
             .subject(pd.getSubject())
             .description(pd.getDescription())
             .generation(pd.getGeneration())
+            .category(Category.valueOf(pd.getCategory()))
             .projectVolunteer(projectVolunteer)
             .projectDonation(projectDonation)
             .build();
@@ -61,10 +65,8 @@ public class ProjectService {
      * 프로젝트 정보 상세 조회(상세 조회)
      * @return
      */
-    public List<GetProjectResDto> getProjectDetail(long projectId){
-        projectRepository.findById(projectId);
-        return null;
+    public GetProjectDetailResDto getProjectDetail(long projectId){
+        Project projects = projectRepository.findById(projectId);
+        return new GetProjectDetailResDto(projects, new ArrayList<>());
     }
-
-
 }
