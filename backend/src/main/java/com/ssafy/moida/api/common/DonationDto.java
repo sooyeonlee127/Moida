f599374332d88a68d1420ab2fe384e2b0778c0e3
LIM SEEUN <ssen.lim@gmail.com>
2023-03-22 05:59:22 +0900

    Feat: 매핑 어노테이션 변경

--- backend/src/main/java/com/ssafy/moida/api/common/DonationDto.java
+++ backend/src/main/java/com/ssafy/moida/api/common/DonationDto.java
@@ -1,29 +1,35 @@
 package com.ssafy.moida.api.common;
 
+import com.ssafy.moida.model.ProjectDonation;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 /**
  * [기부 정보를 담은 dto]
  * 기부 일자(시작-종료), 모금액(현재, 목표), 소제목, 설명 + 기부 아이디
  */
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DonationDto {
     private Long id;
-    private String startDate;
-    private String endDate;
+    private Date startDate;
+    private Date endDate;
     private Long targetAmount;
     private Long amount;
     private String subject;
     private String description;
 
-    public DonationDto(String startDate, String endDate, Long targetAmount, Long amount,
-        String subject,
-        String description) {
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.targetAmount = targetAmount;
-        this.amount = amount;
-        this.subject = subject;
-        this.description = description;
+    public DonationDto(ProjectDonation pd){
+        this.id = pd.getId();
+        this.startDate = pd.getStartDate();
+        this.endDate = pd.getEndDate();
+        this.targetAmount = pd.getTargetAmount();
+        this.amount = pd.getAmount();
+        this.subject = pd.getSubject();
+        this.description = pd.getDescription();
     }
 }
--- backend/src/main/java/com/ssafy/moida/api/common/VolunteerDto.java
+++ backend/src/main/java/com/ssafy/moida/api/common/VolunteerDto.java
@@ -1,12 +1,17 @@
 package com.ssafy.moida.api.common;
 
+import com.ssafy.moida.model.ProjectVolunteer;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  * [봉사 정보를 담은 dto]
  * 난이도, 일자(시작-종료), 위치, 소제목, 설명 + 봉사 고유 아이디
  */
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class VolunteerDto {
     private Long id;
     private double difficultyLevel;
@@ -16,12 +21,13 @@ public class VolunteerDto {
     private String subject;
     private String description;
 
-    public VolunteerDto(double difficultyLevel, String startDate, String endDate, String location, String subject, String description) {
-        this.difficultyLevel = difficultyLevel;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.location = location;
-        this.subject = subject;
-        this.description = description;
+    public VolunteerDto(ProjectVolunteer pv){
+        this.id = pv.getId();
+        this.startDate = pv.getStartDate().toString();
+        this.endDate = pv.getEndDate().toString();
+        this.difficultyLevel = pv.getDifficultyLevel();
+        this.location = pv.getLocation();
+        this.subject = pv.getSubject();
+        this.description = pv.getDescription();
     }
 }
