abbb88bc50fd498e36b5eb87b7e3fce1da09a4e4
m5nacar5n <m5nacar5n@gmail.com>
2023-03-22 01:12:11 +0900

    Feat: WalletCreate 컴포넌트 테스트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import NavBar from "./components/NavBar";
+import WalletCreate from "./components/BlockChain/WalletCreate";
 import "./App.css";
 
-
 function App() {
-
-  return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+    return (
+        <div className="App">
+            <NavBar />
+            <WalletCreate />
+        </div>
+    );
 }
 
 export default App;
