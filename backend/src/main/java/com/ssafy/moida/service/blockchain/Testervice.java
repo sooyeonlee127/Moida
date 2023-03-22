d89c23290aa9a0733ac3e6951b8bae2b7068d4e3
limse0204 <limse0204@naver.com>
2023-03-22 16:27:17 +0900

    Merge: Merge Branch

--- backend/src/main/java/com/ssafy/moida/service/blockchain/Testervice.java
+++ backend/src/main/java/com/ssafy/moida/service/blockchain/Testervice.java
@@ -1,30 +1,30 @@
-package com.ssafy.moida.service.blockchain;
-
-import com.ssafy.moida.model.Users;
-import com.ssafy.moida.model.blockchain.Wallet;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.crossstore.ChangeSetPersister;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-/**
- * 블록체인 테스트 전용 서비스
- */
-
-@Service
-@Transactional
-@RequiredArgsConstructor
-@Slf4j
-public class TestService {
-    private final PasswordEncoder passwordEncoder;
-    private final WalletService walletService;
-    Users saveOrUpdate(Users user) throws ChangeSetPersister.NotFoundException {
-//        isValid(user); // user 유효성 검사
-        Wallet wallet = walletService.createAccount(user)
-                .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
-        userContractService.registUser(wallet.getAddress());
-        user.encodePassword(this.passwordEncoder);
-    }
-}
+//package com.ssafy.moida.service.blockchain;
+//
+//import com.ssafy.moida.model.Users;
+//import com.ssafy.moida.model.blockchain.Wallet;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.data.crossstore.ChangeSetPersister;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+///**
+// * 블록체인 테스트 전용 서비스
+// */
+//
+//@Service
+//@Transactional
+//@RequiredArgsConstructor
+//@Slf4j
+//public class TestService {
+//    private final PasswordEncoder passwordEncoder;
+//    private final WalletService walletService;
+//    Users saveOrUpdate(Users user) throws ChangeSetPersister.NotFoundException {
+////        isValid(user); // user 유효성 검사
+//        Wallet wallet = walletService.createAccount(user)
+//                .orElseThrow(() -> new ChangeSetPersister.NotFoundException());
+//        userContractService.registUser(wallet.getAddress());
+//        user.encodePassword(this.passwordEncoder);
+//    }
+//}
