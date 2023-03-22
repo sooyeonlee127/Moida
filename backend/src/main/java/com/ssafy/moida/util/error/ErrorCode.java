935c0565ebd999cdb7ae191d9f2faf32df442a56
한선영 <gyeoul98@naver.com>
2023-03-22 14:52:55 +0900

    Comment: 400 에러 설명 주석 추가

--- backend/src/main/java/com/ssafy/moida/util/error/ErrorCode.java
+++ backend/src/main/java/com/ssafy/moida/util/error/ErrorCode.java
@@ -16,6 +16,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     /* 404 NOT_FOUND : DATA를 찾을 수 없음 */
     DATA_NOT_FOUND(NOT_FOUND, "일치하는 데이터가 없습니다."),
+
+    /* 400 BAD_REQUEST : 클라이언트의 요청이 유효하지 않아 더 이상 작업을 진행하지 않는 경우 */
     INVALID_PASSWORD(BAD_REQUEST, "유효하지 않은 비밀번호입니다.");
 
     private final HttpStatus httpStatus;
