d90b85bfa47acca3ab75b10b8caae1c8103b3db8
한선영 <gyeoul98@naver.com>
2023-03-22 11:53:07 +0900

    Feat: UserRepository에 JpaRepository 상속

--- backend/src/main/java/com/ssafy/moida/repository/UserRepository.java
+++ backend/src/main/java/com/ssafy/moida/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.moida.repository;
 
+import com.ssafy.moida.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * User 데이터베이스와 통신하는 클래스
  */
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<Users, Long> {
 
 }
