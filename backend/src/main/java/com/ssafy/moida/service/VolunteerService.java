affe1abd535b29727ba7e21eb90c6ea0c4719bf9
limse0204 <limse0204@naver.com>
2023-03-22 13:23:17 +0900

    Feat: 일시 type 변경

--- backend/src/main/java/com/ssafy/moida/service/VolunteerService.java
+++ backend/src/main/java/com/ssafy/moida/service/VolunteerService.java
@@ -1,16 +1,13 @@
 package com.ssafy.moida.service;
 
-import com.ssafy.moida.api.common.DonationDto;
 import com.ssafy.moida.api.common.VolunteerDto;
-import com.ssafy.moida.model.ProjectDonation;
 import com.ssafy.moida.model.ProjectVolunteer;
 import com.ssafy.moida.repository.VolunteerRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 /**
  * 봉사 기능 - 봉사 DB 테이블 사이의 브릿지
@@ -25,17 +22,16 @@ public class VolunteerService {
     }
 
     public ProjectVolunteer save(VolunteerDto vd){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
+        // 봉사 테이블에 추가
         ProjectVolunteer projectVolunteer = ProjectVolunteer.builder()
-                .startDate(LocalDateTime.parse(vd.getStartDate(), formatter))
-                .endDate(LocalDateTime.parse(vd.getEndDate(), formatter))
+                .startDate(LocalDateTime.of(LocalDate.parse(vd.getStartDate()), LocalTime.MIDNIGHT))
+                .endDate(LocalDateTime.of(LocalDate.parse(vd.getEndDate()), LocalTime.MAX))
                 .difficultyLevel(vd.getDifficultyLevel())
                 .location(vd.getLocation())
+                .capacityPerDate(vd.getCapacityPerDate())
                 .subject(vd.getSubject())
                 .description(vd.getDescription())
                 .build();
-
         volunteerRepository.save(projectVolunteer);
         return projectVolunteer;
     }
