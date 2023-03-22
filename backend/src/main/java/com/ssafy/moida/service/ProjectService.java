d29e84458c1bfc1caed607949519c9d3ce511eaf
limse0204 <limse0204@naver.com>
2023-03-22 13:24:16 +0900

    Feat: 프로젝트 상세 조회 추가

--- backend/src/main/java/com/ssafy/moida/service/ProjectService.java
+++ backend/src/main/java/com/ssafy/moida/service/ProjectService.java
@@ -1,8 +1,6 @@
 package com.ssafy.moida.service;
 
-import com.ssafy.moida.api.common.DonationDto;
 import com.ssafy.moida.api.common.ProjectDto;
-import com.ssafy.moida.api.common.VolunteerDto;
 import com.ssafy.moida.api.request.CreateProjectReqDto;
 import com.ssafy.moida.api.response.GetProjectDetailResDto;
 import com.ssafy.moida.api.response.GetProjectResDto;
@@ -11,8 +9,9 @@ import com.ssafy.moida.model.Project;
 import com.ssafy.moida.model.ProjectDonation;
 import com.ssafy.moida.model.ProjectVolunteer;
 import com.ssafy.moida.repository.ProjectRepository;
-import com.ssafy.moida.repository.VolunteerRepository;
 
+import com.ssafy.moida.util.error.ErrorCode;
+import com.ssafy.moida.util.exception.CustomException;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class ProjectService {
     private final ProjectRepository projectRepository;
     @Autowired
@@ -37,9 +36,11 @@ public class ProjectService {
         ProjectDto pd = createProjectReqDto.getProjectDto();
 
         // 기부 데이터베이스에 저장
+        // 만약에 저장이 안될 경우, 예외 처리 후 이후 로직 실행을 막아야 함.
         ProjectDonation projectDonation = donationService.save(createProjectReqDto.getDonationDto());
 
         // 봉사 데이터베이스에 저장
+        // 만약에 저장이 안될 경우, 예외 처리 후 이후 로직 실행을 막아야 함.
         ProjectVolunteer projectVolunteer = volunteerService.save(createProjectReqDto.getVolunteerDto());
 
         // 프로젝트 데이터베이스에 저장
@@ -51,6 +52,9 @@ public class ProjectService {
             .projectVolunteer(projectVolunteer)
             .projectDonation(projectDonation)
             .build();
+
+        // 봉사일시 데이터베이스 저장
+
 
         projectRepository.save(project);
     }
@@ -66,10 +70,11 @@ public class ProjectService {
 
     /**
      * 프로젝트 정보 상세 조회(상세 조회)
-     * @return
+     * @return GetProjectDetailResDto
      */
-    public GetProjectDetailResDto getProjectDetail(long projectId){
-        Project projects = projectRepository.findById(projectId);
+    public GetProjectDetailResDto getProjectDetail(Long projectId){
+        Project projects = projectRepository.findById(projectId)
+            .orElseThrow(() -> new CustomException(ErrorCode.DATA_NOT_FOUND));
         return new GetProjectDetailResDto(projects, new ArrayList<>());
     }
 }
