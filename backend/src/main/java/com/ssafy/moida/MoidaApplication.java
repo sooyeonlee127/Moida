81bbcbe35ca7b60f7fecb64806751e57da3d9a8f
limse0204 <limse0204@naver.com>
2023-03-22 15:44:45 +0900

    Feat: aws s3 빈 추가

--- backend/src/main/java/com/ssafy/moida/MoidaApplication.java
+++ backend/src/main/java/com/ssafy/moida/MoidaApplication.java
@@ -9,7 +9,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class MoidaApplication {
-
+	// aws s3 관련 설정 : 지연 문제 해결
+	static {
+		System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MoidaApplication.class, args);
 	}
