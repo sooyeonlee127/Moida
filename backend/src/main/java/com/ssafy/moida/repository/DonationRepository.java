3bd1885de546718fcfbac204638c9111b01f2817
limse0204 <limse0204@naver.com>
2023-03-22 10:27:44 +0900

    Feat: @Repository 빈 주입

--- backend/src/main/java/com/ssafy/moida/repository/DonationRepository.java
+++ backend/src/main/java/com/ssafy/moida/repository/DonationRepository.java
@@ -2,10 +2,12 @@ package com.ssafy.moida.repository;
 
 import com.ssafy.moida.model.ProjectDonation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * 기부 레포지토리
  */
+@Repository
 public interface DonationRepository extends JpaRepository<ProjectDonation, Long> {
 
 }
