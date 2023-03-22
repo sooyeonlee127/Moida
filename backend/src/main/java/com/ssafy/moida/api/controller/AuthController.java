1c6e42d7a17c1772023eb66675f71863b3672778
한선영 <gyeoul98@naver.com>
2023-03-22 11:51:23 +0900

    Feat: AuthController 내 회원가입 기능 삭제

--- backend/src/main/java/com/ssafy/moida/api/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/moida/api/controller/AuthController.java
@@ -1,34 +1,32 @@
 package com.ssafy.moida.api.controller;
 
-import com.ssafy.moida.api.request.UserJoinReqDto;
+import com.ssafy.moida.api.common.LoginDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name="회원인증")
 @RestController
 @RequestMapping("/")
 public class AuthController {
 
-    // 회원가입
-    @Operation(summary = "회원가입", description = "회원 가입을 합니다.")
-    @PostMapping(consumes = {
-            MediaType.APPLICATION_JSON_VALUE
-    })
-    public ResponseEntity<?> join(
-            @RequestPart(value = "info", required = true) UserJoinReqDto userJoinReqDto) {
+    @Operation(summary = "로그인", description = "로그인을 합니다.")
+    @PostMapping(
+            path = "/login",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> login(
+            @RequestBody LoginDto loginDto
+    ) {
+        //로그인
 
-        return new ResponseEntity<>("회원가입 완료", HttpStatus.OK);
+        return new ResponseEntity<>("로그인", HttpStatus.OK);
     }
 
-    // 로그인
-
     // 로그아웃
+
 
 }
