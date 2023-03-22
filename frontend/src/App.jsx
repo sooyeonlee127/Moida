624d95811dbd3d642f29ad7f7bb4fab6f04b1be8
itmakesmesoft <dmsgur7112@gmail.com>
2023-03-22 14:20:18 +0900

    Feat: 기부 프로젝트 목록 라우터 연결

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import DonationPage from "./pages/DonationPage";
+import DonationDetailPage from "./pages/DonationDetailPage";
 import GatchaPage from "./pages/GatchaPage";
 import PointPage from "./pages/PointPage";
 import ProfilePage from "./pages/ProfilePage";
@@ -18,6 +19,7 @@ function App() {
         <Route path={"/login"} element={<LoginPage />} />
         <Route path={"/"} element={<MainPage />} />
         <Route path={"/donation"} element={<DonationPage />} />
+        <Route path={"/donation/:donationid"} element={<DonationDetailPage />} />
         <Route path={"/gatcha"} element={<GatchaPage />} />
         <Route path={"/point"} element={<PointPage />} />
         <Route path={"/profile"} element={<ProfilePage />} />
