bbab04bb0b223cd094d99f66d9675e59b1f0dd2d
임세은 <limse0204@naver.com>
2023-03-23 15:27:58 +0900

    Feat: swagger 문서 작성

--- backend/src/main/java/com/ssafy/moida/api/common/ProjectDto.java
+++ backend/src/main/java/com/ssafy/moida/api/common/ProjectDto.java
@@ -1,20 +1,24 @@
 package com.ssafy.moida.api.common;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
  * [세은] 프로젝트 자체 dto
  * 카테고리, 주제, 차수, 간단설명 + 프로젝트 아이디
  */
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProjectDto {
     private Long id;
+    @Schema(description = "카테고리", defaultValue = "CRANE")
     private String category;
+    @Schema(description = "프로젝트명", defaultValue = "다람쥐와 도토리")
     private String subject;
+    @Schema(description = "프로젝트 대략의 설명", defaultValue = "다람쥐는 오늘도 도토리를 찾아 헤맵니다...")
     private String description;
 
     public ProjectDto(String category, String subject, String description) {
--- backend/src/main/java/com/ssafy/moida/api/common/VolunteerDto.java
+++ backend/src/main/java/com/ssafy/moida/api/common/VolunteerDto.java
@@ -1,7 +1,9 @@
 package com.ssafy.moida.api.common;
 
 import com.ssafy.moida.model.project.ProjectVolunteer;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,17 +11,25 @@ import lombok.NoArgsConstructor;
  * [세은] 봉사 정보를 담은 dto
  * 난이도, 일자(시작-종료), 위치, 매일 가능한 봉사인원, 소제목, 설명 + 봉사 고유 아이디
  */
-@Getter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class VolunteerDto {
+    @Schema(description = "고유 아이디", readOnly = false)
     private Long id;
+    @Schema(description = "시작 날짜", defaultValue = "2023-03-28")
     private String startDate;
+    @Schema(description = "종료 날짜", defaultValue = "2023-04-10")
     private String endDate;
+    @Schema(description = "봉사 난이도", defaultValue = "5.0")
     private double difficultyLevel;
+    @Schema(description = "봉사 위치", defaultValue = "제주특별자치도 제주시 첨단로 242")
     private String location;
+    @Schema(description = "일별 수용 인원", defaultValue = "15")
     private int capacityPerDate;
+    @Schema(description = "소제목", defaultValue = "다람쥐에게 도토리를 전달해요")
     private String subject;
+    @Schema(description = "상세 설명", defaultValue = "먹이가 필요한 다람쥐에게 도토리를 전달하는 봉사입니다")
     private String description;
 
     public VolunteerDto(ProjectVolunteer pv){
