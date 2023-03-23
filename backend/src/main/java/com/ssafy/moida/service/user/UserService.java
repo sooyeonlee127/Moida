5f1acdf98bf1d710079c6c701236844cffc24239
한선영 <gyeoul98@naver.com>
2023-03-23 10:12:31 +0900

    Feat: userService 내 회원가입시 특별한 경우가 아닐 때 일반 유저로 가입

--- backend/src/main/java/com/ssafy/moida/service/user/UserService.java
+++ backend/src/main/java/com/ssafy/moida/service/user/UserService.java
@@ -36,12 +36,12 @@ public class UserService {
      * 유저 role이 "ROLE_ADMIN"으로 들어 왔을 때만 관리자로 회원가입, 그 외에는 일반유저로 회원가입
      * @param userJoinReqDto
      * */
-    public void JoinUser(UserJoinReqDto userJoinReqDto) {
+    public void joinUser(UserJoinReqDto userJoinReqDto) {
         //user role 확인
         if(userJoinReqDto.getRole().equals("ROLE_ADMIN")) {
             userJoinReqDto.setRole(Role.valueOf("ROLE_ADMIN"));
         } else {
-            userJoinReqDto.setRole(Role.valueOf("ROLE_ADMIN"));
+            userJoinReqDto.setRole(Role.valueOf("ROLE_USER"));
         }
 
         Users u = Users.builder().email(userJoinReqDto.getEmail())
@@ -62,7 +62,7 @@ public class UserService {
      * [한선영] 닉네임 중복 검사
      * @param nickname
      * */
-    public void DuplicatedUserByNickname(String nickname) {
+    public void duplicatedUserByNickname(String nickname) {
         // 닉네임이 존재하면 true, 아니라면 false
         boolean userNickname = userRepository.existsByNickname(nickname);
 
@@ -77,7 +77,7 @@ public class UserService {
      * [한선영] 비밀번호 정규 표현식 검사
      * @param password
      * */
-    public void VaildUserByPassword(String password) {
+    public void vaildUserByPassword(String password) {
         // 영문대소문자, 숫자, 특수문자 조합 8자이상 16자 이내
         Pattern pwdPattern = Pattern.compile("^(?=.*[a-zA-Z])(?=.*\\d)(?=.*\\W).{8,16}$");
         Matcher pwdMatcher = pwdPattern.matcher(password);
