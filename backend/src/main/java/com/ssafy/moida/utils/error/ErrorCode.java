22b6fe9ab7057e6108903327237620a5bc042928
임세은 <limse0204@naver.com>
2023-03-23 16:48:56 +0900

    Feat: UNAUTHORIZED_USER 추가

--- backend/src/main/java/com/ssafy/moida/utils/error/ErrorCode.java
+++ backend/src/main/java/com/ssafy/moida/utils/error/ErrorCode.java
@@ -3,6 +3,7 @@ package com.ssafy.moida.utils.error;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +14,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     /* [세은] 404 NOT_FOUND : DATA를 찾을 수 없음 */
     DATA_NOT_FOUND(NOT_FOUND, "일치하는 데이터가 없습니다."),
+
+    /* [세은] 401 UNAUTHORIZED : 권한이 없는 요청이 들어온 경우 */
+    UNAUTHORIZED_USER(UNAUTHORIZED, "권한이 없는 요청입니다."),
 
     /* [한선영] 400 BAD_REQUEST : 클라이언트의 요청이 유효하지 않아 더 이상 작업을 진행하지 않는 경우 */
     INVALID_PASSWORD(BAD_REQUEST, "유효하지 않은 비밀번호입니다."),
