d89c01f2924dc881267ffdb20228bb4bda0e35a6
임세은 <limse0204@naver.com>
2023-03-23 17:23:10 +0900

    Feat: 공지사항 req dto 작성

--- backend/src/main/java/com/ssafy/moida/api/request/CreateBoardReqDto.java
+++ backend/src/main/java/com/ssafy/moida/api/request/CreateBoardReqDto.java
@@ -9,6 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateBoardReqDto {
+    @Schema(description = "제목", defaultValue = "다람쥐 1기 프로젝트")
+    private String subject;
     @Schema(description = "내용", defaultValue = "봉사 인증입니다.")
     private String description;
+    @Schema(description = "프로젝트 아이디", defaultValue = "1")
+    private Long projectId;
 }
