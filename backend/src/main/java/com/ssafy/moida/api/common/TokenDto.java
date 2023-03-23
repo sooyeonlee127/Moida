03d5b58f4378586695dc658f3fb96d98a6003080
한선영 <gyeoul98@naver.com>
2023-03-23 14:19:42 +0900

    Feat: Token 정보를 가져오기 위해 @Getter 추가

--- backend/src/main/java/com/ssafy/moida/api/common/TokenDto.java
+++ backend/src/main/java/com/ssafy/moida/api/common/TokenDto.java
@@ -1,12 +1,14 @@
 package com.ssafy.moida.api.common;
 
 import lombok.Builder;
+import lombok.Getter;
 
 /**
  * [토큰 정보 dto]
  * 인증타입, 엑세스 토큰, 토큰만료시간, 리프레시 토큰
  * */
 
+@Getter
 @Builder
 public class TokenDto {
     private String grantType;
