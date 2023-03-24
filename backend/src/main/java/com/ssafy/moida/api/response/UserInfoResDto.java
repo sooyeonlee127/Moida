ad59bd1f1fa2465de19babbb5b633e606a9d0caa
한선영 <gyeoul98@naver.com>
2023-03-24 11:11:33 +0900

    Feat: 마이페이지 user 정보 보내주는 dto 생성

--- backend/src/main/java/com/ssafy/moida/api/response/UserInfoResDto.java
+++ backend/src/main/java/com/ssafy/moida/api/response/UserInfoResDto.java
@@ -4,5 +4,10 @@ package com.ssafy.moida.api.response;
  * 유저 개인정보 조회 Dto
  */
 public class UserInfoResDto {
+    private String email;
+    private String nickname;
+    private int ticketCnt;
+    private Long point;
+    private String nftUrl;
 
 }
