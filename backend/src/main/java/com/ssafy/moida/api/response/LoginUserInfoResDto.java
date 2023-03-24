f2f75406b5acf8eca1b522681111031f3ed53c83
한선영 <gyeoul98@naver.com>
2023-03-24 10:16:28 +0900

    Feat: 로그인 후 네브바에 보여지는 User 정보 dto 생성

---
+++ backend/src/main/java/com/ssafy/moida/api/response/LoginUserInfoResDto.java
@@ -0,0 +1,18 @@
+package com.ssafy.moida.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * [한선영] 로그인 후 네브바에 보여지는 User 정보
+ * 티켓갯수, 포인트
+ * */
+
+@Getter
+@AllArgsConstructor
+public class LoginUserInfoResDto {
+    private int ticketCnt;
+    private Long point;
+    private String msg;
+
+}
