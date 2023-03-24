7ecd5f6d558dac901c9a58f2ce0b557c5bf3632b
임세은 <limse0204@naver.com>
2023-03-24 14:47:10 +0900

    Feat: 봉사 신청 에러코드 추가

--- backend/src/main/java/com/ssafy/moida/utils/error/ErrorCode.java
+++ backend/src/main/java/com/ssafy/moida/utils/error/ErrorCode.java
@@ -12,11 +12,18 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    /* [세은] 404 NOT_FOUND : DATA를 찾을 수 없음 */
-    DATA_NOT_FOUND(NOT_FOUND, "일치하는 데이터가 없습니다."),
-
+    /* [세은] 400 BAD_REQUEST : 클라이언트의 요청이 유효하지 않아 더 이상 작업을 진행하지 않는 경우 */
+    INVALID_CLIENT_TOKEN(BAD_REQUEST, "유효하지 않은 클라이언트 토큰입니다."),
+    EXCEED_MAX_CAPACITY(BAD_REQUEST, "봉사 신청 인원을 초과하였습니다."),
+    INVALID_VOLUNTEER_DATE(BAD_REQUEST, "해당 일자에 이미 봉사를 신청하였습니다"),
     /* [세은] 401 UNAUTHORIZED : 권한이 없는 요청이 들어온 경우 */
     UNAUTHORIZED_USER(UNAUTHORIZED, "권한이 없는 요청입니다."),
+    /* [세은] 404 NOT_FOUND : DATA를 찾을 수 없음 */
+    DATA_NOT_FOUND(NOT_FOUND, "일치하는 데이터가 없습니다."),
+    /*[세은] 409 CONFLICT : */
+
+
+
 
     /* [한선영] 400 BAD_REQUEST : 클라이언트의 요청이 유효하지 않아 더 이상 작업을 진행하지 않는 경우 */
     INVALID_PASSWORD(BAD_REQUEST, "유효하지 않은 비밀번호입니다."),
