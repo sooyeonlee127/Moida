84276c27aff83e6e07eac2f27868c74dd73f907d
한선영 <gyeoul98@naver.com>
2023-03-24 11:11:53 +0900

    Feat: 로그인 된 유저 정보 가져오기

--- backend/src/main/java/com/ssafy/moida/api/controller/UserController.java
+++ backend/src/main/java/com/ssafy/moida/api/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.ssafy.moida.api.controller;
 
 import com.ssafy.moida.api.request.UserJoinReqDto;
+import com.ssafy.moida.auth.PrincipalDetails;
+import com.ssafy.moida.model.user.Users;
 import com.ssafy.moida.service.user.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -9,6 +11,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
@@ -66,7 +69,13 @@ public class UserController {
     @GetMapping(
             path = "/me"
     )
-    public ResponseEntity<?> getUserDetail () {
+    public ResponseEntity<?> getUserDetail (
+            @AuthenticationPrincipal PrincipalDetails principal
+    ) {
+        // 로그인 된 유저 정보 가져오기
+        Users user = userService.findByEmail(principal.getUsername());
+
+
 
         return new ResponseEntity<>("my", HttpStatus.OK);
     }
