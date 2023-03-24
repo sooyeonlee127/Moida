8fc0e02e05bac6b0b95725131b9f8374c11024de
문희주 <m5nacar5n@gmail.com>
2023-03-24 16:56:05 +0900

    Feat: sepolia testnet 추가

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -3,4 +3,12 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/a4636fca95804d9ab953276a4ea93748",
+      accounts: [
+        "b0148045d850bc0d896cf9b99603fcfdde940b61daf07d5c93c20cf675ac575f",
+      ],
+    },
+  },
 };
