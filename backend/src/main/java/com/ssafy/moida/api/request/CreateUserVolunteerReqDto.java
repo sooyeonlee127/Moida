92cb1bc4d370eccab8e0f9448632f8cb145e30e3
임세은 <limse0204@naver.com>
2023-03-24 11:05:14 +0900

    Feat: 사용자 봉사 신청 req dto

---
+++ backend/src/main/java/com/ssafy/moida/api/request/CreateUserVolunteerReqDto.java
@@ -0,0 +1,11 @@
+package com.ssafy.moida.api.request;
+
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateUserVolunteerReqDto {
+    private Long projectId;
+
+}
