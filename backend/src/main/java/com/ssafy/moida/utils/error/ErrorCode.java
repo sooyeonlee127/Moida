cba443ada37c605129ca0b25cb75eafbb59f5f87
LIM SEEUN <ssen.lim@gmail.com>
2023-03-25 18:56:48 +0900

    Feat: INVALID_DTO_STATUS 추가

--- backend/src/main/java/com/ssafy/moida/utils/error/ErrorCode.java
+++ backend/src/main/java/com/ssafy/moida/utils/error/ErrorCode.java
@@ -21,6 +21,7 @@ public enum ErrorCode {
     DATA_NOT_FOUND(NOT_FOUND, "일치하는 데이터가 없습니다."),
     /*[세은] 409 CONFLICT : */
     DUPLICATE_VOLUNTEER_REGISTER(CONFLICT, "해당 일자에 이미 봉사를 신청하였습니다"),
+    INVALID_DTO_STATUS(CONFLICT, "서버 동작 상태와 클라이언트 요청 상태가 일치하지 않아 작업을 처리할 수 없습니다"),
 
     /* [한선영] 400 BAD_REQUEST : 클라이언트의 요청이 유효하지 않아 더 이상 작업을 진행하지 않는 경우 */
     INVALID_PASSWORD(BAD_REQUEST, "유효하지 않은 비밀번호입니다."),
