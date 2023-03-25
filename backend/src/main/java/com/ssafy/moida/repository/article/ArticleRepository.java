312a513929cc8a20edae667929110fa0665cf7a5
LIM SEEUN <ssen.lim@gmail.com>
2023-03-25 18:57:22 +0900

    Feat: 프로젝트에 따른 게시글 갯수 추가

--- backend/src/main/java/com/ssafy/moida/repository/article/ArticleRepository.java
+++ backend/src/main/java/com/ssafy/moida/repository/article/ArticleRepository.java
@@ -2,11 +2,14 @@ package com.ssafy.moida.repository.article;
 
 import com.ssafy.moida.model.article.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     void deleteById(Long id);
-
     boolean existsById(Long id);
+    @Query("SELECT COUNT(a) FROM Article a WHERE a.project.id = :projectId")
+    Long countByProjectId(@Param("projectId") Long projectId);
 }
