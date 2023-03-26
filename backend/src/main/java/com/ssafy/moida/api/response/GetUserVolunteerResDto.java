f3290dd5647a1c4f43d346b1b02222296aae0dee
한선영 <gyeoul98@naver.com>
2023-03-26 18:29:09 +0900

    Feat: 봉사 내역 dto 생성

---
+++ backend/src/main/java/com/ssafy/moida/api/response/GetUserVolunteerResDto.java
@@ -0,0 +1,30 @@
+package com.ssafy.moida.api.response;
+
+import com.ssafy.moida.model.project.Status;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+/**
+ * [한선영] 봉사 내역 dto
+ * 봉사Id, 프로젝트Id, 프로젝트명, 기수, 봉사일시, 상태
+ * */
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class GetUserVolunteerResDto {
+
+    private Long volunteerId;
+    private Long projectId;
+    private String projectSubject;
+    private int generation;
+    private LocalDateTime regDate;
+    private Status status;
+
+
+}
