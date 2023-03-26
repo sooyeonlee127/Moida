fb6122a7f8ad748e57049da618124d98de4d8d79
한선영 <gyeoul98@naver.com>
2023-03-26 18:29:53 +0900

    Feat: GetUserVolunteerResDto에 맞는 정보 가져오기

--- backend/src/main/java/com/ssafy/moida/repository/user/UsersVolunteerRepository.java
+++ backend/src/main/java/com/ssafy/moida/repository/user/UsersVolunteerRepository.java
@@ -1,9 +1,14 @@
 package com.ssafy.moida.repository.user;
 
+import com.ssafy.moida.api.response.GetUserVolunteerResDto;
 import com.ssafy.moida.model.project.VolunteerDateInfo;
 import com.ssafy.moida.model.user.UsersVolunteer;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +17,15 @@ public interface UsersVolunteerRepository extends JpaRepository<UsersVolunteer, 
     boolean existsByVolunteerDateInfo(VolunteerDateInfo volunteerDateInfo);
     boolean existsById(UsersVolunteer usersVolunteer);
     Optional<UsersVolunteer> findById(Long id);
+
+    @Query("select new com.ssafy.moida.api.response.GetUserVolunteerResDto(" +
+            "uv.id," +
+            "uv.volunteerDateInfo.project.id," +
+            "uv.volunteerDateInfo.project.subject," +
+            "uv.volunteerDateInfo.project.generation," +
+            "uv.regDate," +
+            "uv.status)" +
+            "from UsersVolunteer uv " +
+            "where uv.users.id = :userId")
+    List<GetUserVolunteerResDto> findVolunteersByUserId(@Param("userId") Long userId);
 }
