77907d26c4cf28b80774595985a84203f5b51b52
한선영 <gyeoul98@naver.com>
2023-03-27 00:48:05 +0900

    Feat: 임시 비밀번호 메일 내용 생성

--- backend/src/main/java/com/ssafy/moida/service/utils/EmailService.java
+++ backend/src/main/java/com/ssafy/moida/service/utils/EmailService.java
@@ -90,6 +90,44 @@ public class EmailService {
     }
 
     /**
+     * [한선영] 임시 비밀번호 이메일 내용 생성
+     * @param email
+     * @return message
+     * */
+    public MimeMessage createForgotPwdMessage(String email) throws MessagingException, UnsupportedEncodingException {
+        // 인증코드 생성
+        code = createKey();
+
+        // 인증코드가 포함된 메일 내용 작성
+        String msg="";
+        msg += "<div style='margin:20px;'>";
+        msg += "<h1> 안녕하세요. </h1>";
+        msg += "<h1> 야생동물 상생 플랫폼 [모이다] 입니다. </h1>";
+        msg += "<br>";
+        msg += "<p>아래 임시 비밀번호를 복사해 입력하여 로그인 해주세요. <p>";
+        msg += "<br>";
+        msg += "<p>감사합니다.<p>";
+        msg += "<br>";
+        msg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msg += "<h3 style='color:RoyalBlue;'>임시 비밀번호입니다.</h3>";
+        msg += "<div style='font-size:130%'>";
+        msg += "CODE : <strong>";
+        msg += code+"</strong><div><br/> ";
+        msg += "</div>";
+
+        //메세지 작성
+        MimeMessage message = emailSender.createMimeMessage();
+
+        message.addRecipients(MimeMessage.RecipientType.TO, email); // 메일 받는 대상
+        message.setSubject("[모이다]회원가입 임시 비밀번호"); // 메일 제목
+        message.setText(msg, "utf-8", "html"); // 메일 내용
+        message.setFrom(new InternetAddress("jikumjikum207@gmail.com","모이다")); // 보내는 사람
+
+        return message;
+
+    }
+
+    /**
      * [한선영] 이메일 전송
      * @param emailMsg
      * @return code
