4d781c588cd38f2b222d2e6d1f7cf67914d90685
이수연 <sooyeon0515@naver.com>
2023-03-27 21:28:09 +0900

    Feat: 관리자 공지글 생성 구현

---
+++ frontend/src/pages/AdminNoticePage/index.jsx
@@ -0,0 +1,222 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+import { useReducer, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+
+const AdminNoticePage = () => {
+  const reducer = (state, action) => {
+    return {
+      ...state,
+      [action.name]: action.value,
+    };
+  };
+  const useInputs = (initialForm) => {
+    const [state, dispatch] = useReducer(reducer, initialForm);
+    const onChange = (e) => {
+      dispatch(e.target);
+    };
+    return [state, onChange];
+  };
+  const { projectid } = useParams();
+  const [files, setFiles] = useState([]);
+  const [state, onChange] = useInputs({
+    subject: "",
+    description: "",
+  });
+  const { subject, description } = state;
+
+  const adminSubmit = () => {
+    console.log(subject, description, projectid);
+    let testData = {
+      subject: subject,
+      description: description,
+      projectId: projectid,
+    };
+    const formData = new FormData();
+    formData.append(
+      "board",
+      new Blob([JSON.stringify(testData)], {
+        type: "application/json",
+      })
+    );
+    formData.append("files", files);
+    axios
+      .post("/api/article/board", formData, {
+        headers: {
+          Authorization: localStorage.getItem("accessToken"),
+          refresh: localStorage.getItem("refreshToken"),
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        alert("공지글 생성 완료했습니다.");
+      })
+      .catch((error) => {
+        const response = error.response.data;
+        console.log(response);
+      });
+  };
+  return (
+    <>
+      <Container>
+        <InnerContainer>
+          <div>
+            <Heading>관리자 페이지</Heading>
+          </div>
+          <AdminForm action="#" method="POST">
+            <InputGroup>
+              <div>
+                <Heading>공지글 생성 페이지</Heading>
+              </div>
+              <div>
+                <InputText htmlFor="subject">subject</InputText>
+                <AdminInput
+                  id="subject"
+                  name="subject"
+                  type="text"
+                  value={subject}
+                  onChange={onChange}
+                />
+              </div>
+              <div>
+                <InputText htmlFor="description">description</InputText>
+                <AdminInput
+                  id="description"
+                  name="description"
+                  type="text"
+                  value={description}
+                  onChange={onChange}
+                />
+              </div>
+            </InputGroup>
+            <InputGroup>
+              <div>
+                <Heading>files</Heading>
+              </div>
+              <div>
+                <InputText htmlFor="files">files</InputText>
+                <AdminInput
+                  id="files"
+                  name="files"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files) {
+                      setFiles([...files, e.target.files[0]]);
+                      console.log(files);
+                    }
+                  }}
+                />
+                <AdminInput
+                  id="files"
+                  name="files"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files) {
+                      setFiles([...files, e.target.files[0]]);
+                      console.log(files);
+                    }
+                  }}
+                />
+                <AdminInput
+                  id="files"
+                  name="files"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files) {
+                      setFiles([...files, e.target.files[0]]);
+                      console.log(files);
+                    }
+                  }}
+                />
+                <AdminInput
+                  id="files"
+                  name="files"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files) {
+                      setFiles([...files, e.target.files[0]]);
+                      console.log(files);
+                    }
+                  }}
+                />
+                <AdminInput
+                  id="files"
+                  name="files"
+                  type="file"
+                  onChange={(e) => {
+                    if (e.target.files) {
+                      setFiles([...files, e.target.files[0]]);
+                      console.log(files);
+                    }
+                  }}
+                />
+              </div>
+            </InputGroup>
+            <div>
+              <SubmitButton
+                type="submit"
+                onClick={(e) => {
+                  e.preventDefault();
+                  adminSubmit();
+                }}
+              >
+                제출하기
+              </SubmitButton>
+            </div>
+          </AdminForm>
+        </InnerContainer>
+      </Container>
+    </>
+  );
+};
+
+const Heading = styled.h2`
+  ${tw`
+  mt-6 text-center text-3xl font-bold tracking-tight text-gray-900
+  `}
+`;
+
+const Container = styled.div`
+  ${tw`
+  flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 
+  `}
+`;
+
+const InnerContainer = styled.div`
+  ${tw`
+  w-full max-w-md space-y-8
+  `}
+`;
+
+const AdminForm = styled.form`
+  ${tw`
+  mt-8 space-y-6
+  `}
+`;
+
+const InputGroup = styled.div`
+  ${tw`
+  -space-y-px rounded-md shadow-sm
+  `}
+`;
+
+const InputText = styled.label`
+  ${tw`
+  block text-sm font-medium leading-6 text-gray-900
+  `}
+`;
+
+const AdminInput = styled.input`
+  ${tw`
+  relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6
+  
+  `}
+`;
+
+const SubmitButton = styled.button`
+  ${tw`
+  w-full h-full bg-yellow-600 py-2 px-10 font-semibold text-black
+  `}
+`;
+export default AdminNoticePage;
