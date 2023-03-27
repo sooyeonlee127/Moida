ea3b450307fc3b5d4435340348898a0d4437349b
itmakesmesoft <dmsgur7112@gmail.com>
2023-03-27 10:16:49 +0900

    Fix: 충돌 수정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,12 +13,9 @@ import ReviewDetailPage from "./pages/ReviewDetailPage";
 import NoticeDetailPage from "./pages/NoticeDetailPage";
 import SignupPage from "./pages/SignupPage";
 import PayResult from "./pages/PointPage/components/PayResult";
-<<<<<<< Updated upstream
 import AdminPage from "./pages/AdminPage";
 import Auth from "./context/Auth";
-=======
 import NavBar from "./components/NavBar/index.jsx"
->>>>>>> Stashed changes
 
 export default function App() {
   return (
