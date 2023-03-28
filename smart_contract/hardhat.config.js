91d8940df63879643e340904f99fc027d253b3ab
문희주 <m5nacar5n@gmail.com>
2023-03-28 14:11:15 +0900

    Feat: hardhat.config.js gasPrice 설정 추가

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -4,11 +4,16 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.18",
   networks: {
+    Hardhat: {
+      url: "https://127.0.0.1:8545",
+      gasPrice: 25000000000
+    },
     sepolia: {
       url: "https://sepolia.infura.io/v3/a4636fca95804d9ab953276a4ea93748",
       accounts: [
         "b0148045d850bc0d896cf9b99603fcfdde940b61daf07d5c93c20cf675ac575f",
       ],
+      gasPrice: 25000000000
     },
   },
 };
