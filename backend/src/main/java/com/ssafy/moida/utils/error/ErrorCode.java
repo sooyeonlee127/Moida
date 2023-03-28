b76fa7ca590590c36381f79e721b1ce02d868f79
LIM SEEUN <ssen.lim@gmail.com>
2023-03-28 22:20:50 +0900

    Feat: INVALID_AUTH_CODE 추가

--- backend/src/main/java/com/ssafy/moida/utils/error/ErrorCode.java
+++ backend/src/main/java/com/ssafy/moida/utils/error/ErrorCode.java
@@ -12,12 +12,15 @@ public enum ErrorCode {
     /* [세은] 400 BAD_REQUEST : 클라이언트의 요청이 유효하지 않아 더 이상 작업을 진행하지 않는 경우 */
     EXCEED_MAX_CAPACITY(BAD_REQUEST, "봉사 신청 인원을 초과하였습니다."),
     EXCEED_USER_POINT(BAD_REQUEST, "사용자 보유 금액보다 기부 금액이 더 큽니다."),
-    /* [세은] 401 UNAUTHORIZED : 인증 문제 */
+    /* [세은] 401 UNAUTHORIZED : 클라이언트가 요청한 리소스에 대한 인증 정보가 없거나 잘못된 경우 */
     INVALID_CLIENT_TOKEN(UNAUTHORIZED, "유효하지 않은 클라이언트 토큰입니다."),
-    UNAUTHORIZED_USER(UNAUTHORIZED, "권한이 없는 요청입니다."),
+    INVALID_AUTH_CODE(UNAUTHORIZED, "클라이언트가 요청한 인증 코드가 데이터 리소스와 일치하지 않습니다."),
+    /* [새은] 403 FORBIDDEN : 클라이언트가 요청한 리소스에 대한 권한이 없는 경우 */
+    FORBIDDEN_USER(FORBIDDEN, "권한이 없는 요청입니다."),
     /* [세은] 404 NOT_FOUND : DATA를 찾을 수 없음 */
     DATA_NOT_FOUND(NOT_FOUND, "일치하는 데이터가 없습니다."),
     CATEGORY_NOT_FOUND(NOT_FOUND, "유효하지 않은 카테고리 값입니다."),
+    STATUS_NOT_FOUND(NOT_FOUND, "유효하지 않은 상태 값입니다."),
     /*[세은] 409 CONFLICT : */
     DUPLICATE_VOLUNTEER_REGISTER(CONFLICT, "해당 일자에 이미 봉사를 신청하였습니다"),
     INVALID_DTO_STATUS(CONFLICT, "서버 동작 상태와 클라이언트 요청 상태가 일치하지 않아 작업을 처리할 수 없습니다"),
