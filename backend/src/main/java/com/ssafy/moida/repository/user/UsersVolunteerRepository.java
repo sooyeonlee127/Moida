e430b877c6c4682675ecf0612c0080cc2c4f2292
임세은 <limse0204@naver.com>
2023-03-29 14:01:51 +0900

    Feat: 사용자 봉사 내역 갯수 조회 추가

--- backend/src/main/java/com/ssafy/moida/repository/user/UsersVolunteerRepository.java
+++ backend/src/main/java/com/ssafy/moida/repository/user/UsersVolunteerRepository.java
@@ -8,7 +8,9 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,4 +21,6 @@ public interface UsersVolunteerRepository extends JpaRepository<UsersVolunteer, 
     Optional<UsersVolunteer> findById(Long id);
     Page<UsersVolunteer> findByUsersOrderByRegDateDesc(Users users, Pageable pageable);
     long countByUsersId(Long userId);
+    @Query("SELECT COUNT(*) FROM UsersVolunteer uv where uv.users.id = :userId")
+    Long countUsersOrderByRegDateDesc(@Param("userId") Long userId);
 }
