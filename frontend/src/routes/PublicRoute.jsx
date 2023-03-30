94d59a3111df0996630614fa657f3aadd05103a0
이수연 <sooyeon0515@naver.com>
2023-03-30 16:56:06 +0900

    Feat: 페이지 접근 권한 검사

---
+++ frontend/src/routes/PublicRoute.jsx
@@ -0,0 +1,15 @@
+// 로그아웃된 사용자만 접근 가능 ( 로그인페이지, 회원가입 페이지 )
+import { useContext } from "react";
+import { AuthContext } from "../context/Auth";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PublicRoute = () => {
+  const { isLogin } = useContext(AuthContext);
+  if (isLogin) {
+    alert("이미 로그인하였습니다.");
+    return <Navigate to={"/"} />;
+  }
+  return <Outlet />;
+};
+
+export default PublicRoute;
