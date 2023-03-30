9cfa099badfef5b18e83df9665622cc3bfbe5f11
문희주 <m5nacar5n@gmail.com>
2023-03-30 17:00:16 +0900

    Feat: web3-react 설정

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,12 +6,23 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { BrowserRouter } from "react-router-dom";
+
+import ReactDOM from "react-dom/client";
+import { Web3ReactProvider } from "@web3-react/core";
+import { Web3Provider } from "@ethersproject/providers";
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider, "any");
+  return library;
+}
+
 const container = document.getElementById("root");
-const root = createRoot(container);
+const root = ReactDOM.createRoot(container);
 const queryClient = new QueryClient();
 root.render(
   // <React.StrictMode>
   <>
+  <Web3ReactProvider getLibrary={getLibrary}>
     <GlobalStyles />
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={true} />
@@ -19,6 +30,7 @@ root.render(
         <App />
       </BrowserRouter>
     </QueryClientProvider>
+    </Web3ReactProvider>
   </>
   // </React.StrictMode>
 );
