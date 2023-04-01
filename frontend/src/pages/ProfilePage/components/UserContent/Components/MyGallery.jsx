a48bca3d09f1a0d1ccaa7961ba558d4f356aaa99
itmakesmesoft <dmsgur7112@gmail.com>
2023-04-01 21:16:37 +0900

    Fix: 프록시 수정

--- frontend/src/pages/ProfilePage/components/UserContent/Components/MyGallery.jsx
+++ frontend/src/pages/ProfilePage/components/UserContent/Components/MyGallery.jsx
@@ -8,11 +8,11 @@ const MyGallery = () => {
   const [pageList, setPageList] = useState([]) // 페이지 번호들을 담을 리스트 생성
   const { data: datas, length, error, loading } = useListApi("volunteer-article", pageNum, pageSize) // length는 페이지네이션 활용 용도 - 이은혁
   const Pagination = () => {
-      const lastPage = parseInt((length+5)/pageSize) || 1 // 데이터 개수가 한 페이지 분량보다 작은경우에도 1이 뜨도록 처리 - 이은혁
-      const tmp = []
-      for (let i=1; i<=lastPage; i++) { // 데이터 갯수에 맞게 페이지 목록에 번호 추가 - 이은혁
-          tmp.push(i)
-      }
+    const lastPage = parseInt((length+5)/pageSize) || 1 // 데이터 개수가 한 페이지 분량보다 작은경우에도 1이 뜨도록 처리 - 이은혁
+    const tmp = []
+    for (let i=1; i<=lastPage; i++) { // 데이터 갯수에 맞게 페이지 목록에 번호 추가 - 이은혁
+      tmp.push(i)
+    }
       setPageList(tmp)
   }
   useEffect(() => {
@@ -20,26 +20,23 @@ const MyGallery = () => {
   }, [length])
   return (
     <>
-
-              {datas.map((data, index) => { 
-                    return (
-                        <div key={index}>
-                            <p>프로젝트 분류 : {data.category}</p>
-                            <p>난이도 : {data.difficultyLevel}</p>
-                            <p>제목 : {data.subject}</p>
-                            <p>작성일시 : {data.regDate}</p>
-                            <p>내용 : {data.description}</p>
-                        </div>
-                    )
-                })}
-
-        <p> 
-          {datas.length === 0 && !loading? "빈 값":""}
-          {loading? "로딩 중":""}
-          {pageList?.map((num, index)=>{ return (<PageBtn onClick={()=> setPageNum(num)}>{num}</PageBtn>) })}
-        </p>
-        
-    </>
+      {datas.map((data, index) => { 
+            return (
+                <div key={index}>
+                    <p>프로젝트 분류 : {data.category}</p>
+                    <p>난이도 : {data.difficultyLevel}</p>
+                    <p>제목 : {data.subject}</p>
+                    <p>작성일시 : {data.regDate}</p>
+                    <p>내용 : {data.description}</p>
+                </div>
+            )
+        })}
+    <p> 
+      {datas.length === 0 && !loading? "빈 값":""}
+      {loading? "로딩 중":""}
+      {pageList?.map((num, index)=>{ return (<PageBtn onClick={()=> setPageNum(num)}>{num}</PageBtn>) })}
+    </p>
+  </>
   )
 }
 
