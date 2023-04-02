63137767db9d57eaf84d248b766686199dad87ba
한선영 <gyeoul98@naver.com>
2023-04-02 17:53:20 +0900

    Feat: NFT 정보 repository 생성

---
+++ backend/src/main/java/com/ssafy/moida/repository/nft/NftRepository.java
@@ -0,0 +1,33 @@
+package com.ssafy.moida.repository.nft;
+
+import com.ssafy.moida.api.response.GetUserDonationResDto;
+import com.ssafy.moida.api.response.GetUserNftResDto;
+import com.ssafy.moida.model.nft.Nft;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NftRepository extends JpaRepository<Nft, Long>,
+        PagingAndSortingRepository<Nft, Long> {
+
+    @Query(
+            value = "SELECT new com.ssafy.moida.api.response.GetUserNftResDto("
+                    + "n.id, "
+                    + "n.metaDataUrl, "
+                    + "n.nftPicture.url, "
+                    + "n.name) "
+                    + "FROM Nft n "
+                    + "WHERE n.users.id = :userId "
+                    + "ORDER BY n.regDate DESC ",
+            countQuery = "SELECT COUNT(*) from Nft nf where nf.users.id = :userId"
+    )
+    Page<GetUserNftResDto> findNftsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+    @Query("select count(*) from Nft n where n.users.id = :userId")
+    Long CountFindNftsByUserId(@Param("userId") Long userId);
+}
