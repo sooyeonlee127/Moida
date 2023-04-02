44d0ad4a11ccd58d1de65671110f08b06d2d69d4
이수연 <sooyeon0515@naver.com>
2023-04-02 19:47:00 +0900

    Design: gatcha 페이지 css

--- frontend/src/pages/GatchaPage/components/CharacterList/index.jsx
+++ frontend/src/pages/GatchaPage/components/CharacterList/index.jsx
@@ -1,1 +1,46 @@
+import styled from "styled-components";
+import tw from "twin.macro";
 
+const CharacterList = () => {
+  return (
+    <Container>
+      <h1>CharacterList</h1>
+      <CharacterBox>
+        <Image></Image>
+        <Image></Image>
+        <Image></Image>
+        <Image></Image>
+        <Image></Image>
+        <Image></Image>
+        <Image></Image>
+        <Image></Image>
+        <Image></Image>
+        <Image></Image>
+      </CharacterBox>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: #fafaf3;
+  height: 89vh;
+  width: 100vw;
+  ${tw`
+  items-center justify-center
+  `}
+`;
+
+const CharacterBox = styled.div`
+  ${tw`
+  grid grid-cols-5 gap-2 ml-10
+  `}
+`;
+
+const Image = styled.div`
+  height: 250px;
+  width: 250px;
+  ${tw`
+  bg-yellow-100
+  `}
+`;
+export default CharacterList;
