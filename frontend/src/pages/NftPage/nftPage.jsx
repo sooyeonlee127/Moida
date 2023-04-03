5fadacd1d2a3e928fe2801a9075d8c6b046f9902
임세은 <limse0204@naver.com>
2023-04-03 14:08:16 +0900

    Feat: ipfs url 변경

--- frontend/src/pages/NftPage/nftPage.jsx
+++ frontend/src/pages/NftPage/nftPage.jsx
@@ -8,7 +8,9 @@ import { create as ipfsHttpClient } from "ipfs-http-client";
 import COMMON_ABI from "../../common/ABI";
 import axios from "axios";
 
-const ipfs = ipfsHttpClient({ host: "ipfs", port: 5001, protocol: "http" });
+const ipfs = ipfsHttpClient({
+  url: "http://ipfs",
+});
 
 const NftPage = () => {
   // [변수] 아이템 (파일, 이름, input 클릭 참조), 작가명, 제목, 아이템 소개, 토큰 ID
