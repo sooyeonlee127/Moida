617b1b2a9887fa84578b41482b17231d1bd0c60f
문희주 <m5nacar5n@gmail.com>
2023-04-06 22:47:01 +0900

    Fix: 어카운트 설정 수정

--- frontend/src/pages/PointPage/components/PayResult.jsx
+++ frontend/src/pages/PointPage/components/PayResult.jsx
@@ -105,7 +105,8 @@ const PayResult = () => {
     const gasLimit = 300000; // gas limit를 지정합니다.
     const chargeTx = {
       from: coinbase,
-      to: account || localStorage.getItem("account"),
+      // to: account || localStorage.getItem("account"),
+      to: account,
       value: Eth, // 원하는 이더 양
       gasLimit: web3.utils.toHex(gasLimit),
       gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
