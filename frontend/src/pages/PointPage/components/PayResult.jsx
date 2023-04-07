9997772dfb85417989e8ddb1efe3719d07a7f283
문희주 <m5nacar5n@gmail.com>
2023-04-07 13:06:19 +0900

    Rollback: 충전 가스 오류 롤백 10819895262f7c444289b17679f555627a84b9451~

--- frontend/src/pages/PointPage/components/PayResult.jsx
+++ frontend/src/pages/PointPage/components/PayResult.jsx
@@ -99,18 +99,16 @@ const PayResult = () => {
   const ChargeGasPoint = useCallback(async () => {
     setFlag(true);
     const coinbase = process.env.REACT_APP_SEPOLIA_ADMIN_PUBLIC_KEY;
-    const Eth = web3.utils.toWei("0.01", "ether");
+    const Eth = web3.utils.toWei("0.0001", "ether");
 
-    // // 가스 설정
-    // const gasPriceGwei = 27;
-    // const gasLimit = 300000;
-    // 이더 전송 
+    // 이더 전송
+    const gasLimit = 300000; // gas limit를 지정합니다.
     const chargeTx = {
       from: coinbase,
       to: account || localStorage.getItem("account"),
       value: Eth, // 원하는 이더 양
-      // gasLimit: web3.utils.toHex(gasLimit),
-      // gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
+      gasLimit: web3.utils.toHex(gasLimit),
+      gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
     };
     const signedTx = await web3.eth.accounts.signTransaction(
       chargeTx,
